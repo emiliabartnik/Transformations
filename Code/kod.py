f7bc9fdc0b4b7c76dc9feeb179914c643804f4c5
weronikaga <01179134@pw.edu.pl>
2024-04-21 21:09:24 +0200

    stworzenie pliku

---
+++ Code/kod.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 21 18:49:52 2024
+
+@author: weron
+"""
+
+class Transformacje_wspolrzednych:
+    def __init__(self):
+        pass
+    
+    def xyz_to_blh(self, x,y,z,elipsoida):
+        pass
+    
+    
+    def blh_to_xyz(self, fi, lam, h, elipsoida):
+        pass
+    
+    
+    def xyz_to_neu(self, x, y, z, x_ref, y_ref, z_ref):
+        pass
+
+    def bl_to_2000(self, fi, lam, h, elipsoida):
+        # Metoda przekształcająca współrzędne BL na 2000
+        pass
+
+    def bl_to_1992(self, fi, lam, h, elipsoida):
+        
+        pass
